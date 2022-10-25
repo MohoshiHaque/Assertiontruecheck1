c1db4d94905a392cf220ae4065abe46b38fb522d
mohoshi <mohoshi.haque@therapbd.net>
2022-10-25 12:25:14 +0600

    Update-1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Assertioncheck a1 =new Assertioncheck();
+        a1.Assertiontruecheck();
+    }
+}
